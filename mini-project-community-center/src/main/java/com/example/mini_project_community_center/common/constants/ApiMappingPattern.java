4d5b54a33f6d5fddb75502376dc5f99f25808783
ses9133 <ses9133@gmail.com>
2025-11-17 16:52:07 +0900

    feat: API_MAPPING_PATTERN 생성

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/constants/ApiMappingPattern.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/constants/ApiMappingPattern.java
@@ -17,9 +17,32 @@ public class ApiMappingPattern {
 
     public static final class Payments {
         private Payments() {}
-        public static final String ROOT = BASE + "/Payments";
+        public static final String ROOT = BASE + "/payments";
         public static final String BY_ID = "/{paymentId}";
         public static final String PAYMENT_BY_ID = ROOT + BY_ID;
         public static final String PAYMENT_REFUND = PAYMENT_BY_ID + "/refund";
     }
+
+    public static final class Centers {
+        private Centers() {}
+        public static final String ROOT = BASE + "/centers";
+        public static final String BY_CENTER_ID = ROOT + "/{centerId}";
+    }
+
+    public static final class Courses {
+        private Courses() {}
+        public static final String ROOT = BASE + "/courses";
+        public static final String BY_COURSE_ID = ROOT + "/{courseId}";
+        public static final String BY_STATUS = BY_COURSE_ID + "/status";
+    }
+
+    public static final class Sessions {
+        private Sessions() {}
+        public static final String SESSIONS = BASE + "/sessions";
+        public static final String BY_SESSION_ID = SESSIONS + "/{sessionId}";
+        public static final String BY_COURSE_ID = Courses.BY_COURSE_ID + "/sessions";
+        public static final String BY_STATUS = BY_SESSION_ID + "/status";
+    }
+
+
 }
