51a15ecac5d74d5b749f56e18baada2cca4d2a30
ses9133 <ses9133@gmail.com>
2025-11-17 17:48:42 +0900

    feat: Front_Api_Mapping 생성

---
+++ mini-project-community-center-front/src/apis/common/routes.ts
@@ -0,0 +1,25 @@
+const ROOT = {
+  ENROLLMENTS: '/enrollements',
+  COURSES: '/courses',
+
+}
+
+
+export const API_ROUTES = {
+  ENROLLMENTS: {
+    ME: ROOT.ENROLLMENTS + '/me' ,
+  },
+
+  PAYMENTS: {
+
+  },
+  CENTERS: {
+
+  },
+  COURSES: {
+
+  },
+  SESSIONS: {
+
+  }
+}
