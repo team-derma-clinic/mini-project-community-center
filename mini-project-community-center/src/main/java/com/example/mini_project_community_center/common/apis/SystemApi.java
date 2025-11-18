786095e81c4d5bc419bba0cedb607f0602e36be1
kimgsun <gsune@kakao.com>
2025-11-18 21:37:08 +0900

    feat: system API endpoint 추가 (BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/SystemApi.java
@@ -0,0 +1,7 @@
+package com.example.mini_project_community_center.common.apis;
+
+public class SystemApi {
+    private SystemApi() {}
+    public static final String HEALTH = ApiBase.BASE + "/health";
+    public static final String INFO = ApiBase.BASE + "/info";
+}
