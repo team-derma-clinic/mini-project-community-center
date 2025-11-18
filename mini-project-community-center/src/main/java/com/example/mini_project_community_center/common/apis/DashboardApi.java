fece13762b5c5bf43a512c3aa28bd2741965141f
kimgsun <gsune@kakao.com>
2025-11-18 21:36:02 +0900

    feat: dashboard API endpoint 추가 (BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/DashboardApi.java
@@ -0,0 +1,7 @@
+package com.example.mini_project_community_center.common.apis;
+
+public class DashboardApi {
+    private DashboardApi() {}
+    public static final String INSTRUCTOR_ME = ApiBase.BASE + "/instructors/me/dashboard";
+    public static final String STAFF = ApiBase.BASE + "/staff/dashboard";
+}
