ee20e126ec85ce846f3ebc6369fd9daff4eb4bb0
kimgsun <gsune@kakao.com>
2025-11-18 21:01:40 +0900

    feat: system API path 추가 (FE)

---
+++ mini-project-community-center-front/src/apis/system/system.path.ts
@@ -0,0 +1,8 @@
+import { BASE } from "../common/base.path";
+
+const SYSTEM_PREFIX = BASE;
+
+export const SYSTEM_PATH = {
+  HEALTH: `${SYSTEM_PREFIX}/health`,
+  INFO: `${SYSTEM_PREFIX}/info`,
+};
