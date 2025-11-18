84d81ccb66d35b2674de8aa53b205e982a4ccb76
ses9133 <ses9133@gmail.com>
2025-11-18 22:08:45 +0900

    feat: CourseSessionApi 수정

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/CourseSessionApi.java
@@ -0,0 +1,7 @@
+package com.example.mini_project_community_center.common.apis;
+
+public class CourseSessionApi {
+    private CourseSessionApi() {}
+
+    public static final String ROOT =  CourseApi.BY_COURSE_ID + "/sessions";
+}
