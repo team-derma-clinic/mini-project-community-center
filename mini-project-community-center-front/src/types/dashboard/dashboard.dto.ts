b50b822cfeb87d31c0c85ed556cd01ff6e106e2d
kimgsun <gsune@kakao.com>
2025-11-19 23:39:11 +0900

    feat: Dashboard dto 생성(FE)

---
+++ mini-project-community-center-front/src/types/dashboard/dashboard.dto.ts
@@ -0,0 +1,10 @@
+export interface SearchInstructorDashboard {
+  from?: string;
+  to?: string;
+}
+
+export interface SearchStaffDashboard {
+  centerId?: number;
+  from?: string;
+  to?: string;
+}
