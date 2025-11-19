c6f5e18b58ec3f80cfb7a4b837034c66771aa4f7
ses9133 <ses9133@gmail.com>
2025-11-19 17:18:16 +0900

    feat: FE course type 생성

---
+++ mini-project-community-center-front/src/types/course/course.enum.type.ts
@@ -0,0 +1,8 @@
+export type CourseCategory = 
+  'ART' | 'MUSIC' | 'COOKING' | 'FITNESS' | 'IT' | 'LANGUAGE';
+
+export type CourseLevel = 
+  'BEGINNER' | 'INTERMEDIATE' | 'ADVANCED';
+
+export type CourseStatus =
+  'OPEN' | 'CLOSED' | 'CANCELED';
