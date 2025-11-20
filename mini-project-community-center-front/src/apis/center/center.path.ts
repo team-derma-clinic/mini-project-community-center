5157f6893c580c3d71db9096e2ca05051e23c990
ses9133 <ses9133@gmail.com>
2025-11-20 16:05:45 +0900

    feat: center api & path 수정(FE)

--- mini-project-community-center-front/src/apis/center/center.path.ts
+++ mini-project-community-center-front/src/apis/center/center.path.ts
@@ -3,13 +3,12 @@ import { BASE } from "../common/base.path";
 const CENTER_PREFIX = `${BASE}/centers`;
 
 export const CENTER_PATH = {
-  ROOT: `${CENTER_PREFIX}`,
+  ROOT: CENTER_PREFIX,
 
-  LIST: `${CENTER_PREFIX}`,
-  CREATE: `${CENTER_PREFIX}`,
+  LIST: CENTER_PREFIX,
+  CREATE: CENTER_PREFIX,
   
   DETAIL: (centerId: number) => `${CENTER_PREFIX}/${centerId}`,
   UPDATE: (centerId: number) => `${CENTER_PREFIX}/${centerId}`,
-  DELETE: (centerId: number) => `${CENTER_PREFIX}/${centerId}`,
   BY_CENTER_ID: (centerId: number) => `${CENTER_PREFIX}/${centerId}`
 }
