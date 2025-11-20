87a97e42f465c526dde3d848a81848ca30787c1e
ses9133 <ses9133@gmail.com>
2025-11-20 22:58:45 +0900

    feat: Center Api 수정

--- mini-project-community-center-front/src/apis/center/center.api.ts
+++ mini-project-community-center-front/src/apis/center/center.api.ts
@@ -1,4 +1,4 @@
-import type { CenterCreateRequest, CenterDetailResponse, CenterListResponse, CenterSearchRequest, CenterUpdateRequest } from "@/types/center/center.dto";
+import type { CenterCreateRequest, CenterDetailResponse, CenterListResponse, CenterSearchParams, CenterUpdateRequest } from "@/types/center/center.dto";
 import { privateApi, publicApi } from "../common/axiosInstance";
 import { CENTER_PATH } from "./center.path";
 import type { ApiResponse } from "@/types/common/ApiResponse";
@@ -9,7 +9,7 @@ export const centerApi = {
     return res.data.data;
   },
 
-  searchCenters: async (params?: CenterSearchRequest): Promise<CenterListResponse> => {
+  searchCenters: async (params?: CenterSearchParams): Promise<CenterListResponse> => {
     const res = await publicApi.get<ApiResponse<CenterListResponse>>(CENTER_PATH.LIST, { params });
     return res.data.data;
   },
