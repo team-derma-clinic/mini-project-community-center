47d2b7d4c432e9546fd16f83457b7e20b02e90de
ses9133 <ses9133@gmail.com>
2025-11-20 15:55:36 +0900

    feat: Session api 수정(FE)

--- mini-project-community-center-front/src/apis/session/session.path.ts
+++ mini-project-community-center-front/src/apis/session/session.path.ts
@@ -3,7 +3,7 @@ import { BASE } from "../common/base.path";
 const SESSION_PREFIX = `${BASE}/sessions`;
 
 export const SESSION_PATH = {
-  ROOT: `${SESSION_PREFIX}`,
+  ROOT: SESSION_PREFIX,
 
   DETAIL: (sessionId: number) => `${SESSION_PREFIX}/${sessionId}`,
   UPDATE: (sessionId: number) => `${SESSION_PREFIX}/${sessionId}`,
