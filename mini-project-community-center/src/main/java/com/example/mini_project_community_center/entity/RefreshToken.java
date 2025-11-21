e51c93e17566be72c5b2aa50dbae9a2a7ca92822
kwonding <wldo8541@naver.com>
2025-11-21 12:36:26 +0900

    feat: Refresh Entity 생성(BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/entity/RefreshToken.java
@@ -0,0 +1,55 @@
+package com.example.mini_project_community_center.entity;
+
+import com.example.mini_project_community_center.entity.base.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(
+        name = "refresh_tokens",
+        indexes = {
+                @Index(name = "idx_refresh_token_login_id", columnList = "login_id")
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "login_id",
+            unique = true,
+            foreignKey = @ForeignKey(name = "fk_refresh_token_login")
+    )
+    private User user;
+
+    @Column(nullable = false, length = 512)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiry;
+
+    @Builder
+    private RefreshToken(User user, String token, Instant expiry) {
+        this.user = user;
+        this.token = token;
+        this.expiry = expiry;
+    }
+
+    public void renew (String newToken, Instant newExpiry) {
+        this.token = newToken;
+        this.expiry = newExpiry;
+    }
+
+    public boolean isExpired() {
+        return Instant.now().isAfter(expiry); // 시간의 경과흐름을 파악하는 Instant
+    }
+}
