c125231c924e3499ea86c31b8a0206824f2fe4f2
kwonding <wldo8541@naver.com>
2025-11-21 10:43:25 +0900

    feat: File api 생성 및 수정(FE)

--- mini-project-community-center-front/src/apis/course/course.file.api.ts
+++ mini-project-community-center-front/src/apis/course/course.file.api.ts
@@ -1,3 +1,4 @@
+import type { CourseFileListDto } from "@/types/course/course.file.dto";
 import { publicApi } from "../common/axiosInstance"
 import { COURSE_FILE_PATH } from "./course.file.path"
 
@@ -9,5 +10,26 @@ export const courseFileApi = {
   );
   return (await res).data;
   },
+
+  getFilesByReview: async(courseId: number) => {
+      const res = await publicApi.get<CourseFileListDto>(
+        COURSE_FILE_PATH.LIST(courseId)
+      );
+      console.log("data: ", res.data);
+      return res.data;
+    },
+  
+    updateBoardFiles: async(fileId: number, formData: FormData) => {
+      const res = await publicApi.put<void>(
+        COURSE_FILE_PATH.UPDATE(fileId),
+        formData,
+        { headers: {"Content-Type": "multipart/form-data"}}
+      );
+      return res.data;
+    },
+  
+    deleteBoardFile: async(fileId: number):Promise<void> => {
+      await publicApi.delete(COURSE_FILE_PATH.DELETE(fileId));
+    },
 
 }
---
+++ mini-project-community-center-front/src/apis/review/review.file.api.ts
@@ -0,0 +1,36 @@
+import type { ReviewFileListDto } from "@/types/review/review.file.dto";
+import { publicApi } from "../common/axiosInstance";
+import { REVIEW_FILE_PATH } from "./review.file.path";
+
+export const reviewApi = {
+  uploadReviewFiles: async(reviewId: number, formData: FormData) => {
+    const res = await publicApi.post<void> (
+      REVIEW_FILE_PATH.POST(reviewId),
+      formData,
+      { headers: {"Content-Type": "multipart/form-data"}}
+    );
+    return res.data;
+  },
+
+  getFilesByReview: async(reviewId: number) => {
+    const res = await publicApi.get<ReviewFileListDto>(
+      REVIEW_FILE_PATH.LIST(reviewId)
+    );
+    console.log("data: ", res.data);
+    return res.data;
+  },
+
+  updateBoardFiles: async(fileId: number, formData: FormData) => {
+    const res = await publicApi.put<void>(
+      REVIEW_FILE_PATH.UPDATE(fileId),
+      formData,
+      { headers: {"Content-Type": "multipart/form-data"}}
+    );
+    return res.data;
+  },
+
+  deleteBoardFile: async(fileId: number):Promise<void> => {
+    await publicApi.delete(REVIEW_FILE_PATH.DELETE(fileId));
+  },
+
+}
