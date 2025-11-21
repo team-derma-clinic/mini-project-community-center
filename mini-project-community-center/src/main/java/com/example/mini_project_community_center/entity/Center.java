79d3b0dc7f624689b4896bb7cab637ed6e6da440
ses9133 <ses9133@gmail.com>
2025-11-21 11:26:32 +0900

    feat: Center Entity 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/entity/Center.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/entity/Center.java
@@ -1,4 +1,46 @@
 package com.example.mini_project_community_center.entity;
 
-public class Center {
+import com.example.mini_project_community_center.entity.base.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "centers",
+        indexes = {
+                @Index(name = "idx_centers_geo", columnList = "latitude, longitude")
+        })
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Center extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public static Center createCenter(String name, String address, Double latitude, Double longitude, String phone) {
+        Center center = new Center();
+        center.name = name;
+        center.address = address;
+        center.latitude = latitude;
+        center.longitude = longitude;
+        center.phone = phone;
+        return center;
+    }
 }
