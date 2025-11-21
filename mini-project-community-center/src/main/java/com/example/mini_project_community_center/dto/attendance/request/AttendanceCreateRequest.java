bc36d6d2b934bc533329acbc8b30f29754ec160a
kimgsun <gsune@kakao.com>
2025-11-21 16:07:12 +0900

    feat: Attendance dto 생성(BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/attendance/request/AttendanceCreateRequest.java
@@ -0,0 +1,17 @@
+package com.example.mini_project_community_center.dto.attendance.request;
+
+import com.example.mini_project_community_center.common.enums.AttendanceStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record AttendanceCreateRequest (
+    @NotNull(message = "sessionId는 필수입니다.")
+    Long sessionId,
+
+    @NotNull(message = "userId는 필수입니다.")
+    Long userId,
+
+    @NotNull(message = "status는 필수입니다.")
+    AttendanceStatus status
+){}
