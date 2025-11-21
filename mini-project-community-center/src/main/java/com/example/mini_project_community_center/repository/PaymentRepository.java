6fcdb13e40397252b7c489b468cb1c87913c29ac
ecde4499 <ecde4499@gmail.com>
2025-11-21 11:26:20 +0900

    enroll-pay repository 생성(BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.example.mini_project_community_center.repository;
+
+import com.example.mini_project_community_center.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+}
