2bab4a2c7f9bc7d5de8bf48d8410b2081bb67950
kimgsun <gsune@kakao.com>
2025-11-22 00:38:47 +0900

    chore: Attendance API 정리(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/AttendanceApi.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/AttendanceApi.java
@@ -3,10 +3,10 @@ package com.example.mini_project_community_center.common.apis;
 public class AttendanceApi {
     private AttendanceApi() {}
     public static final String ROOT = ApiBase.BASE + "/attendance";
-    public static final String UPSERT = ROOT;
-    public static final String BY_ID = ROOT + "/{attendanceId}";
-    public static final String DETAIL = BY_ID;
-    public static final String UPDATE = BY_ID;
-    public static final String BY_COURSE = ApiBase.BASE + CourseApi.BY_COURSE_ID + "/attendance";
-    public static final String BY_SESSION = ApiBase.BASE + SessionApi.BY_SESSION_ID + "/attendance";
+
+    public static final String ID_ONLY = ROOT + "/{attendanceId}";
+    public static final String BY_ID = ID_ONLY;
+
+    public static final String COURSE_LIST = ApiBase.BASE + CourseApi.BY_COURSE_ID + "/attendance";
+    public static final String SESSION_LIST = ApiBase.BASE + SessionApi.BY_SESSION_ID + "/attendance";
 }
