219ca612b7017d7cd27051162ac531077ced9dcd
ses9133 <ses9133@gmail.com>
2025-11-21 12:51:57 +0900

    feat: CourseInstructor Entity 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/request/CourseCreateRequest.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/request/CourseCreateRequest.java
@@ -1,4 +1,4 @@
 package com.example.mini_project_community_center.dto.course.request;
 
-public class CourseRequestDto {
+public record CourseCreateRequest() {
 }
---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/request/CourseStatusUpdateRequest.java
@@ -0,0 +1,4 @@
+package com.example.mini_project_community_center.dto.course.request;
+
+public record CourseStatusUpdateRequest() {
+}
