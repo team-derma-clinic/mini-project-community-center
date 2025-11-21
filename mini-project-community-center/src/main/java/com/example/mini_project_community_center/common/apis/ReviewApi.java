c68e9b10edddd4d3b4f069829e8177f7a43fbff8
kimgsun <gsune@kakao.com>
2025-11-22 00:32:50 +0900

    chore: Review API 통합 정리(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/ReviewApi.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/ReviewApi.java
@@ -3,10 +3,10 @@ package com.example.mini_project_community_center.common.apis;
 public class ReviewApi {
     private ReviewApi() {}
     public static final String ROOT = ApiBase.BASE + "/reviews";
-    public static final String CREATE = ROOT;
-    public static final String BY_ID = ROOT + "/{reviewId}";
-    public static final String UPDATE = BY_ID;
-    public static final String DELETE = BY_ID;
-    public static final String ME = ROOT + "/me";
-    public static final String BY_COURSE = ApiBase.BASE + CourseApi.BY_COURSE_ID + "/reviews";
+
+    public static final String ID_ONLY = ROOT + "/{reviewId}";
+    public static final String BY_ID = ID_ONLY;
+
+    public static final String MY_REVIEWS = ROOT + "/me";
+    public static final String COURSE_REVIEWS = ApiBase.BASE + CourseApi.BY_COURSE_ID + "/reviews";
 }
