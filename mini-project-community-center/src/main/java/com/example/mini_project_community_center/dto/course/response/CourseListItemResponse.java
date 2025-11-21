44e58a4eae99a8a6e432354473e45b4044b010e7
ses9133 <ses9133@gmail.com>
2025-11-21 16:25:37 +0900

    feat: Course Dto 수정(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/response/CourseListItemResponse.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/response/CourseListItemResponse.java
@@ -2,6 +2,10 @@ package com.example.mini_project_community_center.dto.course.response;
 
 import com.example.mini_project_community_center.common.enums.CourseCategory;
 import com.example.mini_project_community_center.common.enums.CourseLevel;
+import com.example.mini_project_community_center.common.utils.DateUtils;
+import com.example.mini_project_community_center.entity.Course;
+
+import java.util.List;
 
 public record CourseListItemResponse (
         Long id,
@@ -11,4 +15,22 @@ public record CourseListItemResponse (
         CourseLevel level,
         String startDate,
         String endDate
-) {}
+) {
+    public static CourseListItemResponse fromEntity(Course course) {
+        return new CourseListItemResponse(
+                course.getId(),
+                course.getCenter().getId(),
+                course.getTitle(),
+                course.getCategory(),
+                course.getLevel(),
+                DateUtils.toKstDateString(course.getStartDate()),
+                DateUtils.toKstDateString(course.getEndDate())
+        );
+    }
+
+    public static List<CourseListItemResponse> from(List<Course> courses) {
+        return courses.stream()
+                .map(CourseListItemResponse::fromEntity)
+                .toList();
+    }
+}
