45c234df091b67a6d61ec74c4cc9d8ebb8e37a40
ses9133 <ses9133@gmail.com>
2025-11-21 09:39:10 +0900

    feat: Time 관련 Config 설정(BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.mini_project_community_center.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
