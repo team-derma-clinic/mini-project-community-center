6acd0a1e70c164a7ae900b31a1a4db9b43e5d961
kwonding <wldo8541@naver.com>
2025-11-22 22:55:42 +0900

    feat: File Repository 생성(BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/repository/file/ReviewFileRepository.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_community_center.repository.file;
+
+import com.example.mini_project_community_center.entity.ReviewFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewFileRepository extends JpaRepository<ReviewFile, Long> {
+List<ReviewFile> findByReviewIdOrderByDisplayOrderAsc(Long ReviewId);
+
+Optional<ReviewFile> findByFileInfoId(Long fileId);
+}
