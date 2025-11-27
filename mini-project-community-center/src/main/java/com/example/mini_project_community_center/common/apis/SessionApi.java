6f3874362c3bb31cc6f61788eda5c0123dc7b2b0
ses9133 <ses9133@gmail.com>
2025-11-27 18:30:06 +0900

    feat: Session Api Mapping Pattern 수정(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/SessionApi.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/common/apis/SessionApi.java
@@ -5,5 +5,6 @@ public class SessionApi {
 
     public static final String ROOT = "/sessions";
     public static final String BY_SESSION_ID = ROOT + "/{sessionId}";
+    public static final String ONLY_BY_ID = "/{sessionId}";
     public static final String STATUS = BY_SESSION_ID + "/status";
 }
