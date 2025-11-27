7319d788c3812653866fb3d9701b043c08b608be
ses9133 <ses9133@gmail.com>
2025-11-27 22:17:12 +0900

    feat: Center Service 작성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/center/CenterService.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/center/CenterService.java
@@ -5,17 +5,18 @@ import com.example.mini_project_community_center.dto.center.request.CenterCreate
 import com.example.mini_project_community_center.dto.center.request.CenterUpdateRequest;
 import com.example.mini_project_community_center.dto.center.response.CenterDetailResponse;
 import com.example.mini_project_community_center.dto.center.response.CenterListItemResponse;
+import com.example.mini_project_community_center.security.UserPrincipal;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 
 public interface CenterService {
-    ResponseDto<CenterDetailResponse> createCenter(@Valid CenterCreateRequest req);
+    ResponseDto<CenterDetailResponse> createCenter(UserPrincipal userPrincipal, @Valid CenterCreateRequest req);
 
     ResponseDto<CenterDetailResponse> getCenterDetail(Long centerId);
 
     ResponseDto<Page<CenterListItemResponse>> getCenters(String q, int page, int size, String sort);
 
-    ResponseDto<CenterDetailResponse> updateCenter(Long centerId, @Valid CenterUpdateRequest req);
+    ResponseDto<CenterDetailResponse> updateCenter(UserPrincipal userPrincipal, Long centerId, @Valid CenterUpdateRequest req);
 
-    ResponseDto<Void> deleteCenter(Long centerId, boolean hardDelete);
+    ResponseDto<Void> deleteCenter(UserPrincipal userPrincipal, Long centerId, boolean hardDelete);
 }
--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/center/impl/CenterServiceImpl.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/center/impl/CenterServiceImpl.java
@@ -9,10 +9,15 @@ import com.example.mini_project_community_center.dto.center.response.CenterDetai
 import com.example.mini_project_community_center.dto.center.response.CenterListItemResponse;
 import com.example.mini_project_community_center.entity.center.Center;
 import com.example.mini_project_community_center.repository.center.CenterRepository;
+import com.example.mini_project_community_center.security.UserPrincipal;
 import com.example.mini_project_community_center.service.center.CenterService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +31,9 @@ public class CenterServiceImpl implements CenterService {
 
     @Transactional
     @Override
-    public ResponseDto<CenterDetailResponse> createCenter(CenterCreateRequest req) {
+    @PreAuthorize("")
+    public ResponseDto<CenterDetailResponse> createCenter(UserPrincipal userPrincipal, CenterCreateRequest req) {
+
         Center center = Center.create(
                 req.name(),
                 req.address(),
@@ -46,14 +53,13 @@ public class CenterServiceImpl implements CenterService {
                 saved.getPhone(),
                 DateUtils.toKstString(saved.getCreatedAt())
         );
-
         return ResponseDto.success(data);
     }
 
     @Override
     public ResponseDto<CenterDetailResponse> getCenterDetail(Long centerId) {
         Center center = centerRepository.findById(centerId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 센터를 찾을 수 없습니다. centerId: " + centerId));
+                .orElseThrow(() -> new EntityNotFoundException("해당 센터가 존재하지 않습니다. centerId: " + centerId));
 
         CenterDetailResponse data = new CenterDetailResponse(
                 centerId,
@@ -64,26 +70,74 @@ public class CenterServiceImpl implements CenterService {
                 center.getPhone(),
                 DateUtils.toKstString(center.getCreatedAt())
         );
-
         return ResponseDto.success(data);
     }
 
     @Override
     public ResponseDto<Page<CenterListItemResponse>> getCenters(String q, int page, int size, String sort) {
-        return null;
+        String[] sortParams = sort.split(",");
+        String sortField = sortParams[0];
+        Sort.Direction direction = Sort.Direction.fromString(sortParams[1]);
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
+
+        Page<Center> pageResult;
+
+        if(q == null || q.isBlank()) {
+            pageResult = centerRepository.findAllActive(pageable);
+        } else {
+            pageResult = centerRepository.searchActiveByName(q, pageable);
+        }
+
+        Page<CenterListItemResponse> data = pageResult.map(center -> new CenterListItemResponse(
+                center.getId(),
+                center.getName(),
+                center.getAddress()
+        ));
+        return ResponseDto.success(data);
     }
 
     @Transactional
     @Override
-    public ResponseDto<CenterDetailResponse> updateCenter(Long centerId, CenterUpdateRequest req) {
-        return null;
+    @PreAuthorize("")
+    public ResponseDto<CenterDetailResponse> updateCenter(UserPrincipal userPrincipal, Long centerId, CenterUpdateRequest req) {
+        Center center = centerRepository.findById(centerId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 센터가 존재하지 않습니다. centerId: " + centerId));
+
+        center.update(
+                req.name(),
+                req.address(),
+                ValueMapper.toBigDecimalOrNull(req.latitude()),
+                ValueMapper.toBigDecimalOrNull(req.longitude()),
+                req.phone()
+        );
+
+        Center updated = centerRepository.save(center);
+
+        CenterDetailResponse data = new CenterDetailResponse(
+                updated.getId(),
+                updated.getName(),
+                updated.getAddress(),
+                ValueMapper.toDoubleOrNull(updated.getLatitude()),
+                ValueMapper.toDoubleOrNull(updated.getLongitude()),
+                updated.getPhone(),
+                DateUtils.toKstString(center.getCreatedAt())
+        );
+        return ResponseDto.success(data);
     }
 
     @Transactional
     @Override
-    public ResponseDto<Void> deleteCenter(Long centerId, boolean hardDelete) {
-        return null;
+    @PreAuthorize("")
+    public ResponseDto<Void> deleteCenter(UserPrincipal userPrincipal, Long centerId, boolean hardDelete) {
+        Center center = centerRepository.findById(centerId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 센터가 존재하지 않습니다. centerId: " + centerId));
+
+        if(hardDelete) {
+            centerRepository.delete(center);
+            return ResponseDto.success(null);
+        }
+        center.deactivate();
+        return ResponseDto.success(null);
     }
-
-
 }
