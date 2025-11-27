32da4d84859023242b1df1764ce2faab57d586eb
ses9133 <ses9133@gmail.com>
2025-11-27 17:38:37 +0900

    feat: Center Controller 생성(BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/controller/center/CenterController.java
@@ -0,0 +1,71 @@
+package com.example.mini_project_community_center.controller.center;
+
+import com.example.mini_project_community_center.common.apis.CenterApi;
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.center.request.CenterCreateRequest;
+import com.example.mini_project_community_center.dto.center.request.CenterUpdateRequest;
+import com.example.mini_project_community_center.dto.center.response.CenterDetailResponse;
+import com.example.mini_project_community_center.dto.center.response.CenterListItemResponse;
+import com.example.mini_project_community_center.service.center.CenterService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(CenterApi.ROOT)
+@RequiredArgsConstructor
+public class CenterController {
+    private final CenterService centerService;
+
+    // 센터 생성 (Staff/Admin)
+    @PostMapping
+    public ResponseEntity<ResponseDto<CenterDetailResponse>> createCenter(
+            @Valid @RequestBody CenterCreateRequest req
+            ) {
+        ResponseDto<CenterDetailResponse> data = centerService.createCenter(req);
+        return ResponseEntity.ok(data);
+    }
+
+    // 센터 단건조회 (public)
+    @GetMapping(CenterApi.BY_CENTER_ID)
+    public ResponseEntity<ResponseDto<CenterDetailResponse>> getCenterDetail(
+            @PathVariable Long centerId
+    ) {
+        ResponseDto<CenterDetailResponse> data = centerService.getCenterDetail(centerId);
+        return ResponseEntity.ok(data);
+    }
+
+    // 센터 목록/검색 q?,page,size,sort (public)
+    @GetMapping
+    public ResponseEntity<ResponseDto<Page<CenterListItemResponse>>> getCenters(
+            @RequestParam(required = false) String q,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "name,asc") String sort
+    ) {
+        ResponseDto<Page<CenterListItemResponse>> data = centerService.getCenters(q, page, size, sort);
+        return ResponseEntity.ok(data);
+    }
+
+    // 센터 수정(STAFF, ADMIN)
+    @PutMapping(CenterApi.BY_CENTER_ID)
+    public ResponseEntity<ResponseDto<CenterDetailResponse>> updateCenter(
+            @PathVariable Long centerId,
+            @Valid @RequestBody CenterUpdateRequest req
+            ) {
+        ResponseDto<CenterDetailResponse> data = centerService.updateCenter(centerId, req);
+        return ResponseEntity.ok(data);
+    }
+
+    // 센터 삭제(권장: 비활성 필드로 대체) (ADMIN)
+    @DeleteMapping(CenterApi.BY_CENTER_ID)
+    public ResponseEntity<ResponseDto<Void>> deleteCenter(
+            @PathVariable Long centerId,
+            @RequestParam(defaultValue = "false") boolean hardDelete) {
+        ResponseDto<Void> data = centerService.deleteCenter(centerId, hardDelete);
+        return ResponseEntity.ok(data);
+    }
+}
