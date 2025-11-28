c32fa56dc7985a7173fd59adaf55871a2884959a
ses9133 <ses9133@gmail.com>
2025-11-28 19:04:35 +0900

    feat: User Dto Mapping 메서드 추가(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/user/response/UserListItemResponse.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/user/response/UserListItemResponse.java
@@ -1,6 +1,8 @@
 package com.example.mini_project_community_center.dto.user.response;
 
 import com.example.mini_project_community_center.common.enums.user.RoleType;
+import com.example.mini_project_community_center.entity.user.User;
+import jakarta.persistence.EntityNotFoundException;
 
 public record UserListItemResponse(
    Long id,
@@ -9,4 +11,20 @@ public record UserListItemResponse(
    String email,
    String phone,
    RoleType role
-) {}
+) {
+    public static UserListItemResponse from(User user) {
+        RoleType role = user.getUserRoles().stream()
+                .map(r -> r.getRole().getName())
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("권한이 존재하지 않습니다."));
+
+        return new UserListItemResponse(
+                user.getId(),
+                user.getName(),
+                user.getLoginId(),
+                user.getEmail(),
+                user.getPhone(),
+                role
+        );
+    }
+}
