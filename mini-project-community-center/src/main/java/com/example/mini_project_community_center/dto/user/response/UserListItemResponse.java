4639aa491e8f16c86dcea67583ed5c5c624fe3f7
ses9133 <ses9133@gmail.com>
2025-11-28 19:53:45 +0900

    feat: User Dto 수정(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/user/response/UserListItemResponse.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/user/response/UserListItemResponse.java
@@ -1,6 +1,7 @@
 package com.example.mini_project_community_center.dto.user.response;
 
 import com.example.mini_project_community_center.common.enums.user.RoleType;
+import com.example.mini_project_community_center.entity.course.CourseInstructor;
 import com.example.mini_project_community_center.entity.user.User;
 import jakarta.persistence.EntityNotFoundException;
 
@@ -24,6 +25,22 @@ public record UserListItemResponse(
                 user.getLoginId(),
                 user.getEmail(),
                 user.getPhone(),
+                role
+        );
+    }
+
+    public static UserListItemResponse fromCourseInstructor(CourseInstructor instructor) {
+        RoleType role = instructor.getInstructor().getUserRoles().stream()
+                .map(r -> r.getRole().getName())
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("권한이 존재하지 않습니다."));
+
+        return new UserListItemResponse(
+                instructor.getInstructor().getId(),
+                instructor.getInstructor().getName(),
+                instructor.getInstructor().getLoginId(),
+                instructor.getInstructor().getEmail(),
+                instructor.getInstructor().getPhone(),
                 role
         );
     }
