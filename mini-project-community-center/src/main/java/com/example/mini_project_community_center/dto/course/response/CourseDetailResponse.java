54031987987f72a6946ad16cfe9cd4bab2294adb
ses9133 <ses9133@gmail.com>
2025-11-28 19:05:04 +0900

    feat: Course Dto - Instructors 필드 추가(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/response/CourseDetailResponse.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/response/CourseDetailResponse.java
@@ -3,7 +3,12 @@ package com.example.mini_project_community_center.dto.course.response;
 import com.example.mini_project_community_center.common.enums.course.CourseCategory;
 import com.example.mini_project_community_center.common.enums.course.CourseLevel;
 import com.example.mini_project_community_center.common.utils.DateUtils;
+import com.example.mini_project_community_center.dto.user.response.UserDetailResponseDto;
+import com.example.mini_project_community_center.dto.user.response.UserListItemResponse;
 import com.example.mini_project_community_center.entity.course.Course;
+import com.example.mini_project_community_center.entity.user.User;
+
+import java.util.List;
 
 
 public record CourseDetailResponse(
@@ -13,9 +18,10 @@ public record CourseDetailResponse(
         CourseCategory category,
         CourseLevel level,
         String startDate,
-        String endDate
+        String endDate,
+        List<UserListItemResponse> instructors
 ) {
-    public static CourseDetailResponse fromEntity(Course course) {
+    public static CourseDetailResponse fromEntity(Course course, List<UserListItemResponse> instructors) {
         return new CourseDetailResponse(
                 course.getId(),
                 course.getCenter().getId(),
@@ -23,7 +29,8 @@ public record CourseDetailResponse(
                 course.getCategory(),
                 course.getLevel(),
                 DateUtils.toKstDateString(course.getStartDate()),
-                DateUtils.toKstDateString(course.getEndDate())
+                DateUtils.toKstDateString(course.getEndDate()),
+                instructors
         );
     }
 }
