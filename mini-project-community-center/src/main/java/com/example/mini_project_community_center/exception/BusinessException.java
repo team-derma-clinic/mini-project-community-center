5a925c3b4738cababd93dd79034c9736385dca0c
ses9133 <ses9133@gmail.com>
2025-11-29 19:58:56 +0900

    feat: Exception & Handler 정의(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/exception/BusinessException.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/exception/BusinessException.java
@@ -1,4 +1,33 @@
 package com.example.mini_project_community_center.exception;
 
-public class BusinessException {
+import com.example.mini_project_community_center.common.enums.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+    private final String detailMessage;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.detailMessage = null;
+    }
+
+    public BusinessException(ErrorCode errorCode, String detailMessage) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.detailMessage = detailMessage;
+    }
+
+    public HttpStatus getStatus() {
+        return errorCode.getStatus();
+    }
+
+    @Override
+    public String toString() {
+        return "[BusinessException] " + errorCode +
+                (detailMessage != null ? " / detail: " + detailMessage : "");
+    }
 }
