32da3873c745f3811f23a48a0d7de6ba6df6466c
kwonding <wldo8541@naver.com>
2025-11-29 15:54:08 +0900

    feat: File ServiceImpl 수정(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/impl/ReviewFileServiceImpl.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/impl/ReviewFileServiceImpl.java
@@ -1,4 +1,38 @@
 package com.example.mini_project_community_center.service.file.impl;
 
-public class ReviewServiceImpl {
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.file.request.ReviewFileUpdateRequestDto;
+import com.example.mini_project_community_center.dto.file.response.ReviewFileListResponseDto;
+import com.example.mini_project_community_center.repository.file.ReviewFileRepository;
+import com.example.mini_project_community_center.service.file.ReviewFileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewFileServiceImpl implements ReviewFileService {
+    private final ReviewFileRepository reviewFileRepository;
+
+    @Override
+    public ResponseDto<Void> uploadReviewFiles(Long reviewId, List<MultipartFile> files) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<List<ReviewFileListResponseDto>> getFilesByReview(Long reviewId) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<Void> deleteReviewFile(Long fileId) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<Void> updateReviewFiles(Long reviewId, ReviewFileUpdateRequestDto dto) {
+        return null;
+    }
 }
