96a2efd06dc572730529c462eaf47bda0de9eee8
kwonding <wldo8541@naver.com>
2025-11-29 15:53:30 +0900

    feat: File Service 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/CourseFileService.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/CourseFileService.java
@@ -1,4 +1,15 @@
-package com.example.mini_project_community_center.service.file.impl;
+package com.example.mini_project_community_center.service.file;
 
-public class CourseFileService {
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.file.response.CourseFileListResponseDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface CourseFileService {
+    ResponseDto<Void> uploadThumbnail(Long courseId, MultipartFile file);
+
+    List<CourseFileListResponseDto> getCourseFiles(Long courseId);
+
+    ResponseDto<Void> deleteCourseFile(Long fileId);
 }
--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/ReviewFileService.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/ReviewFileService.java
@@ -1,4 +1,18 @@
-package com.example.mini_project_community_center.service.file.impl;
+package com.example.mini_project_community_center.service.file;
 
-public class ReviewFileService {
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.file.request.ReviewFileUpdateRequestDto;
+import com.example.mini_project_community_center.dto.file.response.ReviewFileListResponseDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ReviewFileService {
+    ResponseDto<Void> uploadReviewFiles(Long reviewId, List<MultipartFile> files);
+
+    ResponseDto<List<ReviewFileListResponseDto>> getFilesByReview(Long reviewId);
+
+    ResponseDto<Void> deleteReviewFile(Long fileId);
+
+    ResponseDto<Void> updateReviewFiles(Long reviewId, ReviewFileUpdateRequestDto dto);
 }
