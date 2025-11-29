45c8ef265d4d433bfa281d7688315668dba67e54
ses9133 <ses9133@gmail.com>
2025-11-29 18:59:12 +0900

    feat: CourseSearchRequest Dto 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/request/CourseSearchRequest.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/course/request/CourseSearchRequest.java
@@ -1,4 +1,42 @@
 package com.example.mini_project_community_center.dto.course.request;
 
-public record CourseSearchRequest() {
-}
+import com.example.mini_project_community_center.common.enums.course.CourseCategory;
+import com.example.mini_project_community_center.common.enums.course.CourseLevel;
+import com.example.mini_project_community_center.common.enums.course.CourseStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record CourseSearchRequest(
+        @Positive(message = "centerId는 양수여야합니다.")
+        Long centerId,
+
+        CourseCategory category,
+        CourseLevel level,
+        CourseStatus status,
+        String from,
+        String to,
+
+        @Min(value = 1, message = "weekday는 1~7범위여야 합니다.")
+        @Max(value = 7, message = "weekday는 1~7범위여야 합니다.")
+        Integer weekday,
+
+        String timeRange,
+
+        @Max(value = 50, message = "검색어는 50자이하여야 합니다.")
+        String q,
+
+        @NotNull(message = "page 값은 필수입니다.")
+        @Min(value = 0, message = "page는 0이상이어야 합니다. ")
+        Integer page,
+
+        @NotNull
+        @Min(value = 1, message = "size는 1이상이어야 합니다.")
+        Integer size,
+
+        @NotNull
+        String sort
+) {}
