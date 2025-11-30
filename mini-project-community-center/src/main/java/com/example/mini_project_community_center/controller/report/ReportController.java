d2cf3a4365d5e6a0f0c73eaeb128aae5fea9fbfa
kimgsun <gsune@kakao.com>
2025-12-01 00:58:47 +0900

    feat: Report Controller 생성(BE)

---
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/controller/report/ReportController.java
@@ -0,0 +1,42 @@
+package com.example.mini_project_community_center.controller;
+
+import com.example.mini_project_community_center.common.apis.ReportApi;
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.report.request.SearchCourseReportRequest;
+import com.example.mini_project_community_center.dto.report.response.CourseReportResponse;
+import com.example.mini_project_community_center.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ReportApi.ROOT)
+@RequiredArgsConstructor
+public class ReportController {
+    private final ReportService reportService;
+
+    // 1. 강좌별 통계(등록/정원/환불)
+    // GET /api/v1/reports/courses
+    @GetMapping(ReportApi.COURSES)
+    public ResponseEntity<ResponseDto<CourseReportResponse>> getCourseReport(
+            @RequestParam(required = false) Long centerId,
+            @RequestParam(required = false) String from,
+            @RequestParam(required = false) String to,
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false) Integer limit
+    ) {
+        SearchCourseReportRequest req = new SearchCourseReportRequest(centerId, from, to, sort, limit);
+        ResponseDto<CourseReportResponse> data = reportService.getCourseReport(req);
+        return ResponseEntity.ok(data);
+    }
+
+    // 2. 출석 통계(세션/강좌/사용자)
+
+    // 3. 카테고리별 통계(등록수/평균펼점/인기순위)
+
+    // 4. 강사별 통계(담당강좌수/평균평점/출석률?
+
+}
