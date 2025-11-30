b50831e770ae72230110e633536a5aef1f97afb2
kwonding <wldo8541@naver.com>
2025-11-30 18:27:12 +0900

    feat: ReviewFile Service 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/impl/ReviewFileServiceImpl.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/file/impl/ReviewFileServiceImpl.java
@@ -1,10 +1,17 @@
 package com.example.mini_project_community_center.service.file.impl;
 
+import com.example.mini_project_community_center.common.enums.error.ErrorCode;
 import com.example.mini_project_community_center.dto.ResponseDto;
 import com.example.mini_project_community_center.dto.file.request.ReviewFileUpdateRequestDto;
 import com.example.mini_project_community_center.dto.file.response.ReviewFileListResponseDto;
+import com.example.mini_project_community_center.entity.file.FileInfo;
+import com.example.mini_project_community_center.entity.review.Review;
+import com.example.mini_project_community_center.entity.review.ReviewFile;
+import com.example.mini_project_community_center.exception.BusinessException;
 import com.example.mini_project_community_center.repository.file.ReviewFileRepository;
+import com.example.mini_project_community_center.repository.review.ReviewRepository;
 import com.example.mini_project_community_center.service.file.ReviewFileService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,10 +21,34 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ReviewFileServiceImpl implements ReviewFileService {
+
+    private final ReviewRepository reviewRepository;
     private final ReviewFileRepository reviewFileRepository;
+    private final FileServiceImpl fileService;
+
+    private final int MAX_ATTACH = 3;
 
     @Override
     public ResponseDto<Void> uploadReviewFiles(Long reviewId, List<MultipartFile> files) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND, "해당 리뷰를 찾을 수 없습니다: " + reviewId));
+
+        if (files.size() > MAX_ATTACH) {
+            throw new IllegalArgumentException("최대 " + MAX_ATTACH + "개까지 업로드 가능합니다.");
+        }
+
+        int order = 0;
+
+        for (MultipartFile file : files) {
+            if (file == null || file.isEmpty()) {
+                continue;
+            }
+            FileInfo info = fileService.saveReviewFile(reviewId, file);
+            if (info == null) continue;
+
+            ReviewFile reviewFile = new ReviewFile(review, info, order++);
+            reviewFileRepository.save(reviewFile);
+        }
         return null;
     }
 
