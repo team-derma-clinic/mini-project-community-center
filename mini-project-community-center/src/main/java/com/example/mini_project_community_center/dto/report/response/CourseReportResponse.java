2ad93df24b24e374ebac140145053133ee4b98dc
kimgsun <gsune@kakao.com>
2025-12-01 02:46:43 +0900

    feat: Report response Dto 필드스펙 수정(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/report/response/CourseReportResponse.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/dto/report/response/CourseReportResponse.java
@@ -2,10 +2,12 @@ package com.example.mini_project_community_center.dto.report.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record CourseReportResponse(
-        List<CourseReportItemResponse> courses
+        Long courseId,
+        String courseName,
+        Integer capacity,
+        Integer refundCount,
+        Double enrollmentRate
 ) {
 }
