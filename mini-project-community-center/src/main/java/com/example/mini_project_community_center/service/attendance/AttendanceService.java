87760ccbfcf0a635a066b5d64f0d7f54ad4c861c
kimgsun <gsune@kakao.com>
2025-12-01 14:43:12 +0900

    feat: Attendance Service 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/attendance/AttendanceService.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/attendance/AttendanceService.java
@@ -1,4 +1,18 @@
 package com.example.mini_project_community_center.service.attendance;
 
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.attendance.request.AttendanceCreateRequest;
+import com.example.mini_project_community_center.dto.attendance.request.AttendanceUpdateRequest;
+import com.example.mini_project_community_center.dto.attendance.response.AttendanceDetailResponse;
+import com.example.mini_project_community_center.security.UserPrincipal;
+import jakarta.validation.Valid;
+
+import java.util.List;
+
 public interface AttendanceService {
+    ResponseDto<AttendanceDetailResponse> upsertAttendance(UserPrincipal userPrincipal, @Valid AttendanceCreateRequest req);
+    ResponseDto<AttendanceDetailResponse> getAttendanceDetail(Long attendanceId);
+    ResponseDto<AttendanceDetailResponse> updateAttendance(UserPrincipal userPrincipal, Long attendanceId, @Valid AttendanceUpdateRequest req);
+    ResponseDto<List<AttendanceDetailResponse>> getCourseAttendance(Long courseId, Long sessionId, Long userId, Integer page, Integer size, String sort);
+    ResponseDto<List<AttendanceDetailResponse>> getSessionAttendance(Long sessionId, Integer page, Integer size, String sort);
 }
