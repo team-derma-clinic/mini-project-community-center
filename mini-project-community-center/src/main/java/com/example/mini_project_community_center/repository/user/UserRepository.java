18f6221a2700cdf53cf39469beaf63fc52c28ecc
kwonding <wldo8541@naver.com>
2025-12-02 00:53:34 +0900

    feat: Repository 추가(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/repository/user/UserRepository.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/repository/user/UserRepository.java
@@ -1,13 +1,21 @@
 package com.example.mini_project_community_center.repository.user;
 
+import com.example.mini_project_community_center.common.enums.user.AuthProvider;
 import com.example.mini_project_community_center.common.enums.user.RoleType;
 import com.example.mini_project_community_center.entity.user.User;
+import com.example.mini_project_community_center.security.user.UserPrincipalMapper;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     List<User> findUserByRole(RoleType role);
+
+    Optional<User> findByLoginId(@NonNull String loginId);
+
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
 }
