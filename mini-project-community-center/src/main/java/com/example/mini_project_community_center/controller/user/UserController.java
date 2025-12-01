ce654e1b792263d69c761390a51b5a765e6dc10c
kwonding <wldo8541@naver.com>
2025-12-01 15:51:57 +0900

    feat: User controller 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/controller/user/UserController.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/controller/user/UserController.java
@@ -2,13 +2,16 @@ package com.example.mini_project_community_center.controller.user;
 
 import com.example.mini_project_community_center.common.apis.UserApi;
 import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.role.RoleRequestDto;
+import com.example.mini_project_community_center.dto.user.request.UserUpdateRequestDto;
 import com.example.mini_project_community_center.dto.user.response.UserDetailResponseDto;
+import com.example.mini_project_community_center.dto.user.response.UserListItemResponse;
+import com.example.mini_project_community_center.security.UserPrincipal;
 import com.example.mini_project_community_center.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(UserApi.ROOT)
@@ -17,5 +20,37 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping(UserApi.ME)
-    public ResponseEntity<ResponseDto<UserDetailResponseDto>>
+    public ResponseEntity<ResponseDto<UserDetailResponseDto>> me(
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
+        ResponseDto<UserDetailResponseDto> data = userService.getMe(userPrincipal.getId());
+        return ResponseEntity.status(data.getStatus()).body(data);
+    }
+
+    @GetMapping(UserApi.BY_ID)
+    public ResponseEntity<ResponseDto<UserDetailResponseDto>> getById(
+            @PathVariable Long userId
+    ){
+        ResponseDto<UserDetailResponseDto> data = userService.getUserById(userId);
+        return ResponseEntity.status(data.getStatus()) .body(data);
+    }
+
+    @PutMapping(UserApi.ME)
+    public ResponseEntity<ResponseDto<UserListItemResponse>> updateUserInfo(
+        @AuthenticationPrincipal UserPrincipal userPrincipal,
+        @RequestBody UserUpdateRequestDto dto
+    ){
+        ResponseDto<UserListItemResponse> data = userService.updateUserInfo(userPrincipal, dto);
+        return ResponseEntity.status(data.getStatus()).body(data);
+    }
+
+    @PutMapping(UserApi.ROLES)
+    public ResponseEntity<ResponseDto<UserListItemResponse>> updateRoles(
+            @PathVariable Long userId,
+            @RequestBody RoleRequestDto dto
+    ){
+      ResponseDto<UserListItemResponse> data = userService.updateRole(userId, dto);
+      return ResponseEntity.status(data.getStatus()).body(data);
+    }
+
 }
