2bfe036f8d30d6850e67991f2aa8a2977cc08eaf
ecde4499 <ecde4499@gmail.com>
2025-12-01 21:37:52 +0900

    feat: enroll-pay controller 생성

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/enrollment/impl/EnrollmentServiceImpl.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/enrollment/impl/EnrollmentServiceImpl.java
@@ -1,4 +1,46 @@
 package com.example.mini_project_community_center.service.enrollment.impl;
 
-public class EnrollmentServiceImpl {
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.enrollment.request.EnrollmentCreateRequest;
+import com.example.mini_project_community_center.dto.enrollment.request.EnrollmentReasonRequest;
+import com.example.mini_project_community_center.dto.enrollment.response.EnrollmentDetailResponse;
+import com.example.mini_project_community_center.dto.enrollment.response.EnrollmentListItemResponse;
+import com.example.mini_project_community_center.security.UserPrincipal;
+import com.example.mini_project_community_center.service.enrollment.EnrollmentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EnrollmentServiceImpl implements EnrollmentService {
+
+    @Override
+    public ResponseDto<List<EnrollmentListItemResponse>> getMyEnrollments(UserPrincipal user) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<EnrollmentDetailResponse> createEnrollment(UserPrincipal userPrincipal, EnrollmentCreateRequest req) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<List<EnrollmentListItemResponse>> getAllEnrollments() {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<EnrollmentDetailResponse> getEnrollmentById(UserPrincipal userPrincipal, Long enrollmentId) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<EnrollmentDetailResponse> cancelEnrollment(UserPrincipal userPrincipal, Long enrollmentId, EnrollmentReasonRequest req) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<EnrollmentDetailResponse> refundEnrollment(UserPrincipal userPrincipal, Long enrollmentId, EnrollmentReasonRequest req) {
+        return null;
+    }
 }
--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/payment/impl/PaymentServiceImpl.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/payment/impl/PaymentServiceImpl.java
@@ -1,4 +1,26 @@
 package com.example.mini_project_community_center.service.payment.impl;
 
-public class PaymentServiceImpl {
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.payment.request.PaymentRequestDto;
+import com.example.mini_project_community_center.dto.payment.response.PaymentResponseDto;
+import com.example.mini_project_community_center.security.UserPrincipal;
+import com.example.mini_project_community_center.service.payment.PaymentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public ResponseDto<PaymentResponseDto> createPayment(UserPrincipal userPrincipal, PaymentRequestDto req) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<PaymentResponseDto> getPaymentById(UserPrincipal userPrincipal, Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<PaymentResponseDto> refundPayment(UserPrincipal userPrincipal, Long paymentId) {
+        return null;
+    }
 }
