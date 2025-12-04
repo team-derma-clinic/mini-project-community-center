ae3407b2e099944b724769f530868bc140ef84ba
kwonding <wldo8541@naver.com>
2025-12-04 15:18:32 +0900

    feat: User service 생성(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/user/UserService.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/user/UserService.java
@@ -1,4 +1,18 @@
 package com.example.mini_project_community_center.service.user;
 
+import com.example.mini_project_community_center.dto.ResponseDto;
+import com.example.mini_project_community_center.dto.role.RoleRequestDto;
+import com.example.mini_project_community_center.dto.user.request.UserUpdateRequestDto;
+import com.example.mini_project_community_center.dto.user.response.UserDetailResponseDto;
+import com.example.mini_project_community_center.dto.user.response.UserListItemResponse;
+import com.example.mini_project_community_center.security.user.UserPrincipal;
+
 public interface UserService {
+    ResponseDto<UserDetailResponseDto> getMe(Long id);
+
+    ResponseDto<UserDetailResponseDto> getUserById(Long userId);
+
+    ResponseDto<UserListItemResponse> updateUserInfo(UserPrincipal userPrincipal, UserUpdateRequestDto dto);
+
+    ResponseDto<UserListItemResponse> updateRole(Long userId, RoleRequestDto dto);
 }
