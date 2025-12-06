741fd931973285f220c3b4429d762cf054b43901
ses9133 <ses9133@gmail.com>
2025-12-06 20:32:39 +0900

    feat: Object Mapper 생성자 주입 방식 변경(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/security/handler/JsonAccessDeniedHandler.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/security/handler/JsonAccessDeniedHandler.java
@@ -5,6 +5,7 @@ import com.example.mini_project_community_center.dto.ResponseDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
@@ -15,9 +16,10 @@ import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JsonAccessDeniedHandler implements AccessDeniedHandler {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(
