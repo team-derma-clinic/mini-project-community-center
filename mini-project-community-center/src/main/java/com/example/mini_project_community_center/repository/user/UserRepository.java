d2d27fb13c09c7a84b3c17c90e7c457251c25694
kwonding <wldo8541@naver.com>
2025-12-06 14:22:23 +0900

    feat: User 수정 (BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/repository/user/UserRepository.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/repository/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.mini_project_community_center.repository.user;
 
 import com.example.mini_project_community_center.common.enums.user.AuthProvider;
+import com.example.mini_project_community_center.common.enums.user.RoleStatus;
 import com.example.mini_project_community_center.common.enums.user.RoleType;
 import com.example.mini_project_community_center.entity.user.User;
 import com.example.mini_project_community_center.security.user.UserPrincipalMapper;
@@ -15,11 +16,13 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
-    List<User> findUserByRole(RoleType role);
+    List<User> findUsersByRole(RoleType role);
 
     Optional<User> findByLoginId(@NonNull String loginId);
 
     Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
 
     Optional<User> findByEmail(@NotBlank(message = "이메일은 필수입니다.") @Email(message = "이메일 형식이 올바르지 않습니다.") String email);
+
+    List<User> findAllByRoleStatus(RoleStatus roleStatus);
 }
