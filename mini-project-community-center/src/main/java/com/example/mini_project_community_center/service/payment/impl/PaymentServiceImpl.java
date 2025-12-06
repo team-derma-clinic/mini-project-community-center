1385a5671aa631532c9d3e4589b4f0879690c47a
ses9133 <ses9133@gmail.com>
2025-12-06 19:35:51 +0900

    feat: UserPrincipal Import 경로 수정(BE)

--- mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/payment/impl/PaymentServiceImpl.java
+++ mini-project-community-center/src/main/java/com/example/mini_project_community_center/service/payment/impl/PaymentServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.mini_project_community_center.service.payment.impl;
 import com.example.mini_project_community_center.dto.ResponseDto;
 import com.example.mini_project_community_center.dto.payment.request.PaymentRequestDto;
 import com.example.mini_project_community_center.dto.payment.response.PaymentResponseDto;
-import com.example.mini_project_community_center.security.UserPrincipal;
+import com.example.mini_project_community_center.security.user.UserPrincipal;
 import com.example.mini_project_community_center.service.payment.PaymentService;
 import org.springframework.stereotype.Service;
 
